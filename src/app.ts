1816814e1301f260d447e05289ce10b9553b7c4b
may h <mehrabhoque@gmail.com>
2021-10-23 19:43:11 -0400

    Fix unused parameter warnings

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get('/', (req: any, res: any) => {
+app.get('/', (_: any, res: any) => {
   res.send('Hello world!');
 });
 
@@ -14,7 +14,7 @@ app.listen(port, () => {
 });
 
 // For the react app to hit.
-app.get('/express_backend', (req: any, res: any) => {
+app.get('/express_backend', (_: any, res: any) => {
   console.log('hello babie!');
   res.send({
     express: ['hello', 'world', 'from', 'express'],
