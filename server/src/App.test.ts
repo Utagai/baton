56ed9efb7de294ad7d2fab0c955729f89a6c10b7
may h <mehrabhoque@gmail.com>
2021-11-01 00:26:55 -0400

    Implement full test for upload

--- server/src/App.test.ts
+++ server/src/App.test.ts
@@ -1,5 +1,8 @@
+import fs from 'fs';
+import path from 'path';
 import request from 'supertest';
 import pino from 'pino';
+import { addDays } from 'date-fns';
 
 import FilesDB from './FilesDB';
 import AppFactory from './AppFactory';
@@ -7,6 +10,7 @@ import AppFactory from './AppFactory';
 const testLogLevel = 'debug';
 const testSQLiteDBFile = './sqlite/baton_test.db';
 const testUploadPath = './uploaded-test/';
+const testDefaultFileLifetime = 1;
 
 function getTestFilesDB(currentTestName: string) {
   const testTableName = currentTestName.replace(/ /g, '_');
@@ -26,10 +30,12 @@ function getTestApp(currentTestName: string) {
   });
   const filesDB = getTestFilesDB(currentTestName);
   const fileUploadPath = testUploadPath;
-  const defaultFileLifetimeInDays = 1;
+  const defaultFileLifetimeInDays = testDefaultFileLifetime;
   return AppFactory(logger, filesDB, fileUploadPath, defaultFileLifetimeInDays);
 }
 
+// Run the clean-up _before_ the tests run, so that on failure, we still have
+// the leftover data in SQLite + disk for debugging purposes.
 beforeEach(() => {
   const { currentTestName } = expect.getState();
   const filesDB = getTestFilesDB(currentTestName);
@@ -46,7 +52,6 @@ test('GET files empty', async () => {
     .expect(200)
     .expect('Content-Type', /json/)
     .then((resp) => {
-      console.log('resp body:', resp.body);
       expect(Array.isArray(resp.body.files)).toBeTruthy();
       expect(resp.body.files.length).toBe(0);
     });
@@ -56,11 +61,11 @@ test('GET files non empty', async () => {
   const { currentTestName } = expect.getState();
   const filesDB = getTestFilesDB(currentTestName);
   const fileToUpload = {
-    name: 'test',
+    name: currentTestName,
     size: 100,
     id: currentTestName,
     uploadTime: new Date(),
-    expireTime: new Date(), // This is a bit weird, but not invalid.
+    expireTime: addDays(new Date(), testDefaultFileLifetime),
   };
   expect(filesDB.addFile(fileToUpload)).toBe(1); // Expect to have this one file's metadata uploaded.
 
@@ -70,22 +75,64 @@ test('GET files non empty', async () => {
     .expect(200)
     .expect('Content-Type', /json/)
     .then((resp) => {
-      console.log('resp body:', resp.body);
       expect(Array.isArray(resp.body.files)).toBeTruthy();
       expect(resp.body.files.length).toBe(1);
       // Ideally, we'd just check .toBe(fileToUpload), but remember that this
       // resp is JSON, so the dates are not Date objects but strings. There are
       // some other approaches, e.g. making a new proper File object or maybe
-      // some custom matcher thing, but this is dead simple and the object is
-      // relatively small.
-      expect(resp.body.files[0].name).toBe(fileToUpload.name);
-      expect(resp.body.files[0].size).toBe(fileToUpload.size);
-      expect(resp.body.files[0].id).toBe(fileToUpload.id);
-      expect(resp.body.files[0].uploadTime).toBe(
-        fileToUpload.uploadTime.toISOString(),
+      // some custom matcher thing, but this is dead simple and the number of
+      // fields to check 'specially' is quite small.
+      expect(resp.body.files[0]).toEqual({
+        ...fileToUpload,
+        uploadTime: fileToUpload.uploadTime.toISOString(),
+        expireTime: fileToUpload.expireTime.toISOString(),
+      });
+    });
+});
+
+test('upload', async () => {
+  const { currentTestName } = expect.getState();
+  const app = getTestApp(expect.getState().currentTestName);
+
+  const dataToUpload = Buffer.from('hello world!');
+  const idToUpload = `${currentTestName}_test.txt`;
+  await request(app)
+    .post('/upload')
+    .field('filename', currentTestName)
+    .field('filesize', '100')
+    .field('id', idToUpload)
+    .attach('file', dataToUpload)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .then((resp) => {
+      console.log('resp body:', resp.body);
+      expect(resp.body).toEqual({
+        name: currentTestName,
+        size: 100,
+        id: idToUpload,
+        uploadTime: expect.any(String),
+        expireTime: expect.any(String),
+      });
+      // Now, be a bit more strict with testing of our dates. We won't expect a
+      // perfect match of course, but we'd like to verify that the server is
+      // setting reasonable upload & expiration times. Namely, that isn't, for
+      // example, setting the expiration time to 500 years in the future even
+      // though our configured expiration time is 1 day.
+      // NOTE: The parameter of -1 we pass to toBeCloseTo() translates to a
+      // generous 5 second tolerance.
+      expect(Date.parse(resp.body.uploadTime) / 1000.0).toBeCloseTo(
+        new Date().getTime() / 1000.0,
+        -1,
       );
-      expect(resp.body.files[0].expireTime).toBe(
-        fileToUpload.expireTime.toISOString(),
+      expect(Date.parse(resp.body.expireTime) / 1000.0).toBeCloseTo(
+        addDays(new Date(), testDefaultFileLifetime).getTime() / 1000.0,
+        -1,
       );
+
+      // Finally, confirm that the data we wanted to upload has been uploaded:
+      const actualData = fs
+        .readFileSync(path.join(testUploadPath, idToUpload), 'utf8')
+        .toString();
+      expect(actualData).toEqual(dataToUpload.toString());
     });
 });
