388b5ae55aabd8e73f0b39b474fcf5ac7e457969
may h <mehrabhoque@gmail.com>
2021-11-06 19:37:20 -0400

    Add tests for the file rows

---
+++ client/src/FileRow.test.tsx
@@ -0,0 +1,45 @@
+import { addDays, formatDuration, intervalToDuration } from 'date-fns';
+import { render, within } from '@testing-library/react';
+import FileRow from './FileRow';
+
+describe('FileRow', () => {
+  test('renders with expected column values', () => {
+    const expectedFile = {
+      id: '0',
+      name: 'file',
+      size: 3029,
+      uploadTime: `2021-11-06T22:57:46.162Z`,
+      // As long as it is in the future, we do not care. We just want the computed
+      // expiration duration to be non-negative.
+      expireTime: addDays(new Date(), 1).toISOString(),
+    };
+
+    const tbody = document.createElement('tbody');
+    const { container } = render(
+      <FileRow f={expectedFile} deleteFile={() => {}} />,
+      {
+        container: document.body.appendChild(tbody),
+      },
+    );
+
+    const fileRowElement = within(container).getByRole('row');
+    expect(fileRowElement).toBeInTheDocument();
+
+    expect(within(container).getByText(expectedFile.name)).toBeInTheDocument();
+    expect(within(container).getByText('(3.03 kB)')).toBeInTheDocument();
+    expect(
+      within(container).getByText('November 6th, 6:57 PM'),
+    ).toBeInTheDocument();
+    expect(
+      within(container).getByText(
+        formatDuration(
+          intervalToDuration({
+            start: new Date(),
+            end: Date.parse(expectedFile.expireTime),
+          }),
+          { format: ['days', 'hours', 'minutes'], delimiter: ', ' },
+        ),
+      ),
+    ).toBeInTheDocument();
+  });
+});
---
+++ client/src/FileRows.test.tsx
@@ -0,0 +1,30 @@
+import { render, within } from '@testing-library/react';
+import FileRows from './FileRows';
+
+describe('FileRows', () => {
+  test('renders correct number of file rows', () => {
+    const numFiles = 10;
+    const files = Array(numFiles)
+      .fill(0)
+      .map((_, i) => ({
+        id: i.toString(),
+        name: i.toString(),
+        size: i,
+        uploadTime: `2021-01-06T22:57:46.162Z`,
+        expireTime: `2021-02-06T22:57:46.162Z`,
+      }));
+    const table = document.createElement('table');
+    const { container } = render(
+      <FileRows files={files} deleteFile={() => {}} />,
+      {
+        container: document.body.appendChild(table),
+      },
+    );
+
+    const fileRowElements = within(container).getAllByRole('row');
+    expect(fileRowElements).toHaveLength(numFiles);
+    fileRowElements.forEach((fileRow) => {
+      expect(fileRow).toBeInTheDocument();
+    });
+  });
+});
