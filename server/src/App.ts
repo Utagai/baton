e57d440af8bfe45800db989cfda9cfecc6c1d658
may h <mehrabhoque@gmail.com>
2021-11-14 23:11:49 -0500

    Make the default log level debug
    This lets us use debug in development. Yay.

--- server/src/App.ts
+++ server/src/App.ts
@@ -6,7 +6,7 @@ import AppFactory from './AppFactory';
 // TODO: These things should be configurable. Environment variables are probably
 // sufficient for all of these, since there aren't many of them.
 // TODO: Should we try to make the logger stay in pretty mode?
-const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const logger = pino({ level: process.env.LOG_LEVEL || 'debug' });
 const filesDB = new SQLiteFilesDB('./sqlite/baton_dev.db');
 const fileUploadPath = './uploaded/';
 const defaultFileLifetimeInDays = 7;
