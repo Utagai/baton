1ad54e696669da62308fa4eb132153a91cc03d08
may h <mehrabhoque@gmail.com>
2021-11-21 22:19:50 -0500

    Clean up App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import debounce from 'lodash.debounce';
 
 import './index.css';
 import Banner from './Banner';
@@ -13,95 +12,131 @@ async function callBackendAPI(endpoint: string, method: string) {
   const body = await response.json();
 
   if (response.status !== 200) {
-    throw Error(body.message);
+    // TODO: This should be handled/propagated better.
+    throw Error(body);
   }
 
   return body;
 }
 
-async function triggerDeletionOfExpiredFiles() {
-  callBackendAPI('deleteexpired', 'DELETE');
-}
-
-async function getCurrentFiles() {
-  await triggerDeletionOfExpiredFiles();
+async function getCurrentFileMetadatas(): Promise<{ files: FileMetadata[] }> {
+  // TODO: We should not be calling /deleteexpired here because it overloads the
+  // responsibilities of this function. We should only be returning /files. The
+  // /deleteexpired should either:
+  //  * Be running in a standalone process on the box. (best option)
+  //  * Be running in the refresh thread.
+  await callBackendAPI('deleteexpired', 'DELETE');
   return callBackendAPI('files', 'GET');
 }
 
-const App = () => {
-  const [files, setFiles] = React.useState<FileMetadata[]>([]);
-  const textInputRef = React.useRef<HTMLDivElement>(null);
-
-  const debounceInterval = 250;
-  const debouncedCall = React.useCallback(
-    debounce(getCurrentFiles, debounceInterval, { leading: true }),
-    [],
+function tableElem(
+  metadatas: FileMetadata[],
+  setMetadatas: (newMetadatas: FileMetadata[]) => void,
+) {
+  return (
+    <Table
+      metadatas={metadatas}
+      deleteMetadataFromState={(metadataId: string) => {
+        setMetadatas(
+          metadatas.filter((metadata) => metadata.id !== metadataId),
+        );
+      }}
+    />
   );
+}
+
+function uploadButtonsElem(
+  metadatas: FileMetadata[],
+  setMetadatas: (newMetadatas: FileMetadata[]) => void,
+  writeAFileOnClick: () => void,
+) {
+  return (
+    <UploadButtons
+      addMetadataToState={(metadata: FileMetadata) => {
+        setMetadatas(metadatas.concat([metadata]));
+      }}
+      writeAFileOnClick={writeAFileOnClick}
+    />
+  );
+}
+
+function customTextElem(
+  metadatas: FileMetadata[],
+  setMetadatas: (newMetadatas: FileMetadata[]) => void,
+  textInputRef: React.RefObject<HTMLDivElement>,
+) {
+  return (
+    <CustomText
+      textInputAreaRef={textInputRef}
+      // TODO: Is this little wrapping code around setFiles being duplicated?
+      addMetadataToState={(metadata: FileMetadata) => {
+        setMetadatas(metadatas.concat([metadata]));
+      }}
+    />
+  );
+}
+
+const App = () => {
+  const [metadatas, setMetadatas] = React.useState<FileMetadata[]>([]);
+  const textInputRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     let mounted = true;
-    const resp = debouncedCall();
-    while (resp === undefined) {
-      throw Error(`backend took too long to respond (> ${debounceInterval})`);
-    }
-    resp
-      .then((res) => {
-        if (res !== undefined && mounted) {
-          setFiles(res.files);
+    getCurrentFileMetadatas()
+      .then((metadata) => {
+        if (metadata !== undefined && mounted) {
+          setMetadatas(metadata.files);
         }
       })
       .catch((err) => {
         throw Error(`failed to get files from backend: ${err}`);
       });
 
+    // We have to return this weird clean-up function because it is possible
+    // that while handling getCurrentFiles()' promise, the component is
+    // unmounted. If that happens, mounted will become false because this
+    // clean-up function is called. Using this boolean then lets us avoid
+    // setting state on an unmounted component.
+    // Finally, note that this is for our root component. However, since baton
+    // is a SPA, this root component will actually _never_ unmount. Therefore,
+    // this code is actually just written to get rid of a warning & there is
+    // no real issue here. If the warning came from a lower-level component,
+    // then the answer would be to move up the state handling to a
+    // higher-level component or use pub/sub.
     return () => {
       mounted = false;
     };
+    // useEffect with a state of [] runs only once, at mount time.
   }, []);
+
+  const writeAFileOnClick = () => {
+    // Toggle the display visibility of the writing section div.
+    if (textInputRef.current === null) {
+      return;
+    }
+
+    const currentVisibilitySetting = textInputRef.current.style.visibility;
+    // I've seen things online where this could sometimes be 'none'
+    // instead of '', so we check for both.
+    if (
+      currentVisibilitySetting === '' ||
+      currentVisibilitySetting === 'hidden'
+    ) {
+      textInputRef.current.style.visibility = 'visible';
+    } else {
+      textInputRef.current.style.visibility = 'hidden';
+    }
+  };
 
   return (
     <div className="grid place-items-center">
       <Banner />
 
-      <Table
-        metadatas={files}
-        deleteMetadataFromState={(id: string) => {
-          setFiles(files.filter((f) => f.id !== id));
-        }}
-      />
+      {tableElem(metadatas, setMetadatas)}
 
-      <UploadButtons
-        addMetadataToState={(f: FileMetadata) => {
-          setFiles(files.concat([f]));
-        }}
-        writeAFileOnClick={() => {
-          // Toggle the display visibility of the writing section div.
-          if (textInputRef.current === null) {
-            return;
-          }
+      {uploadButtonsElem(metadatas, setMetadatas, writeAFileOnClick)}
 
-          const currentVisibilitySetting =
-            textInputRef.current.style.visibility;
-          // I've seen things online where this could sometimes be 'none'
-          // instead of '', so we check for both.
-          if (
-            currentVisibilitySetting === '' ||
-            currentVisibilitySetting === 'hidden'
-          ) {
-            textInputRef.current.style.visibility = 'visible';
-          } else {
-            textInputRef.current.style.visibility = 'hidden';
-          }
-        }}
-      />
-
-      <CustomText
-        textInputAreaRef={textInputRef}
-        // TODO: Is this little wrapping code around setFiles being duplicated?
-        addMetadataToState={(f: FileMetadata) => {
-          setFiles(files.concat([f]));
-        }}
-      />
+      {customTextElem(metadatas, setMetadatas, textInputRef)}
     </div>
   );
 };
