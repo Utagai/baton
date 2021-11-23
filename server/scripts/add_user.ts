84bc39f0f032795cb64cb78735374ed9d26af6f5
may h <mehrabhoque@gmail.com>
2021-11-23 18:37:25 -0500

    Add a script for creating users on the database

---
+++ server/scripts/add_user.ts
@@ -0,0 +1,33 @@
+import 'process';
+
+import { createPasswordHash } from '../src/Password';
+import { SQLiteUsersDB } from '../src/UsersDB';
+
+// Just some sloppy validation...
+const MAX_ARGS = 5;
+
+if (process.argv.length !== MAX_ARGS) {
+  console.error(
+    `invalid number of arguments (${process.argv.length}); npm run adduser -- <sqlite db file> <new username> <password>`,
+  );
+  process.exit(1);
+}
+
+const sqliteDBFilepath = process.argv[2];
+const newUsername = process.argv[3];
+const plaintextPassword = process.argv[4];
+
+console.log(
+  `Going to create ${newUsername}, with password: ${'*'.repeat(
+    plaintextPassword.length,
+  )} @ ${sqliteDBFilepath}`,
+);
+
+const passwordHashInfo = createPasswordHash(plaintextPassword);
+
+const usersDB = new SQLiteUsersDB(sqliteDBFilepath, 'users');
+
+usersDB.addUser({
+  username: newUsername,
+  passwordHashInfo,
+});
